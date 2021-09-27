dec296219a029b3d0ac26de3f8e37f7009887ec7
grisalesangela <angelagrisales1@gmail.com>
2021-09-27 18:18:17 -0500

    :recycle: Hello, World

---
+++ 0x02-shell_redirections/create.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+touch 0-hello_world 1-confused_smiley 2-hellofile 3-twofiles 4-lastlines 5-firstlines 6-third_line 7-file 8-cwd_state
+touch 9-duplicate_last_line 10-no_more_js 11-directories 12-newest_files 13-unique 14-findthatword 15-countthatword 16-whatsnext 17-hidethisword 18-letteronly 19-AZ 20-hiago 21-reverse 22-users_and_homes
+echo "#!/bin/bash" > 0-hello_world
+echo "#!/bin/bash" > 1-confused_smiley
+echo "#!/bin/bash" > 2-hellofile
+echo "#!/bin/bash" > 3-twofiles 
+echo "#!/bin/bash" > 4-lastlines 
+echo "#!/bin/bash" > 5-firstlines 
+echo "#!/bin/bash" > 6-third_line 
+echo "#!/bin/bash" > 7-file 
+echo "#!/bin/bash" > 8-cwd_state
+echo "#!/bin/bash" > 9-duplicate_last_line 
+echo "#!/bin/bash" > 10-no_more_js 
+echo "#!/bin/bash" > 11-directories 
+echo "#!/bin/bash" > 12-newest_files 
+echo "#!/bin/bash" > 13-unique 
+echo "#!/bin/bash" > 14-findthatword 
+echo "#!/bin/bash" > 15-countthatword 
+echo "#!/bin/bash" > 16-whatsnext 
+echo "#!/bin/bash" > 17-hidethisword 
+echo "#!/bin/bash" > 18-letteronly 
+echo "#!/bin/bash" > 19-AZ 
+echo "#!/bin/bash" > 20-hiago 
+echo "#!/bin/bash" > 21-reverse 
+echo "#!/bin/bash" > 22-users_and_homes
+chmod +x *
